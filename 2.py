67f300dd1292e756b00c0c0a6b074096ba48d1bd
Your Name <you@example.com>
2018-11-15 08:45:09 +0100

    nothing

---
+++ 2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 28 23:09:11 2018
+
+@author: kuba
+"""
+
+birthday = input('Kiedy masz urodziny? ')
+print('Twoje urodziny obchodzone sa: ' + birthday)
+
+
